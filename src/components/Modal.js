25940d70eaca03669de977e35cdc4684063e4727
David Sozashvili <davidsozashvili@gmail.com>
2022-11-25 16:48:55 +0400

    caseSencitive to false

---
+++ src/components/Modal.js
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+const ModalWrapper = styled.div`
+    position: absolute;
+    width: 100vw;
+    height: 100%;
+`
+
+const BackDrop = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: lightgrey;
+    opacity: 0.5;
+`
+
+const Content = styled.div`
+    position: absolute;
+    top: 100px;
+    left: calc(50% - 250px);
+    padding: 10px;
+    border-radius: 10px;
+    width: 500px;
+    height: 400px;
+    background-color: #fff;
+    box-sizing: border-box;
+`
+
+function Modal({children, closeModal}) {
+    
+    return (
+        <ModalWrapper>
+            <BackDrop onClick={closeModal}/>
+            <Content>
+                {children}
+            </Content>
+        </ModalWrapper>
+    )
+}
+
+export default Modal;
